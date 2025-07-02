85105dbc5cce051891aac29ad4a996c5278d0761
Marcelo Raineri <mraineridev@gmail.com>
2025-07-02 19:25:50 -0300

    feat: implement order management endpoints

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -1,12 +1,13 @@
 import { store, get, destroy, update, search} from "../controllers/orderController";
 import { Router } from "express";
-
+import { auth } from "../middlewares/auth";
 const app = Router();
 
-app.get("/", search);
-app.post("/", store);
-app.get("/:id", get);
-app.delete("/:id", destroy);
-app.put("/:id", update);
+
+app.get("/", auth, search);
+app.post("/", auth, store);
+app.get("/:id", auth, get);
+app.delete("/:id", auth, destroy);
+app.put("/:id", auth, update);
 
 export default app;
