28fe4ac007c5d9cad299cc86693a1f9ff5c8beec
Marcelo Raineri <mraineridev@gmail.com>
2025-07-02 19:25:50 -0300

    feat: update application configuration

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import router from './routes/router';
-
+import { auth } from './middlewares/auth';
 const app = express();
 
 app.use(express.json());
+// app.use(auth);
 app.use(router)
 
 export default app;
