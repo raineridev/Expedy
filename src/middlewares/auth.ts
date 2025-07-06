016c9998e0a379eb8066f3d9c0a920f5c5b8de24
Marcelo Raineri <mraineridev@gmail.com>
2025-07-06 16:30:06 -0300

    feat: implement JWT authentication middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,16 +1,21 @@
 import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from "express";
+import { RefreshBodyType } from '../types/refreshBodyType';
+import { UserType } from '../types/userType';
 
 export function auth(req : Request, res : Response, next: NextFunction) {
-    const accessToken   = req.headers['authorization'];
+    const accessToken  = req.headers['authorization'];
     if(!accessToken) {
         res.status(401).json({ error: 'Unauthorized' });
         throw new Error('Unauthorized');
     } 
     try {
-        const decodedToken = jwt.verify(accessToken, process.env.JWT_SECRET as string);
+        const decodedToken = jwt.verify(accessToken, process.env.JWT_SECRET as string) as UserType; 
+        if ((decodedToken as any as RefreshBodyType).type === 'refresh') {
+            res.status(401).json({ error: 'Unauthorized' });
+        }
         console.log('[Middleware]Decoded Token:', decodedToken);
-        // (req.headers as any)['x-user-id'] = decodedToken.user;
+        (req.headers as any)['x-user-id'] = decodedToken.id;
         next();
     } catch (error) {
         console.log(error);
