b0b2061b38a5c280f46700b8c5e74423b7611e8a
Marcelo Raineri <mraineridev@gmail.com>
2025-07-06 16:30:06 -0300

    feat: enhance store service business logic

--- src/services/storeService.ts
+++ src/services/storeService.ts
@@ -1,8 +1,12 @@
 import {store , getStoreById, deleteById, update} from '../repositories/storeRepository';
 import { StoreType } from '../types/storeType';
+import { updateUser } from './userService';
 
-export async function createStore(data: StoreType) {
-    return await store(data);
+export async function createStore(data: StoreType, userId: number) {
+    const storeData = await store(data);
+    const userData =  await updateUser(userId, { store: storeData.id });
+    
+    return storeData;
 }
 
 export async function getStore(id: number) {
