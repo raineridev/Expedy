5fb14b943a69b9db544168a3c20457fe4fcc74ba
Marcelo Raineri <mraineridev@gmail.com>
2025-07-08 06:36:31 -0300

    feat: improve Excel report generation with styling and date formatting

--- src/services/reportService.ts
+++ src/services/reportService.ts
@@ -1,7 +1,9 @@
+import { setDateFormatToData } from "../../utils/DateFormat";
+
 const ExcelJS = require('exceljs');
 const path = require('path');
 
-export async function setWorkbook(creator: string, lastModifiedBy: string, ) {
+export function setWorkbook(creator: string, lastModifiedBy: string, ) {
     const workbook = new ExcelJS.Workbook();
     workbook.creator = creator;
     workbook.lastModifiedBy = lastModifiedBy;
@@ -10,47 +12,39 @@ export async function setWorkbook(creator: string, lastModifiedBy: string, ) {
     return workbook;
 }
 
-export async function setWorksheet(workbook: any, name: string, worksheetData: Array<object>) {
+export function setWorksheet(workbook: any, name: string, worksheetData: Array<object>) {
     const worksheet = workbook.addWorksheet(name);
     const columns = worksheetData;
-    console.log('Columns:', columns);
     worksheet.columns = columns;
 
     return worksheet;
 }
     
 
-export async function setColumns(worksheet: any, data: Array<object>) {
-     worksheet.addRow(...data);
+export function setColumns(worksheet: any, data: Array<object>) {
+    data.forEach((dataRow : Object, i) => {
+        setDateFormatToData(dataRow, 'DD/MM/YYYY - HH:mm', 'data_venda');
+        worksheet.addRow(dataRow);
+    });
 }
+
+export function setStyleColumns(worksheet: any) {
+    worksheet.getRow(1).alignment = { vertical: 'middle', horizontal: 'center' };
+    worksheet.getRow(1).font = { bold: true};
+    worksheet.getRow(1).views = [{ state: 'frozen', ySplit: 1 }];
+    for (let i = 2; i <= worksheet.columnCount; i++) {
+        if((i % 2) === 0) {
+         const col = worksheet.getRow(i);
+        col.width = 20;
+        col.style = { alignment: { horizontal: 'center' } };
+        col.fill = { type: 'pattern', pattern: 'solid', fgColor: { argb: 'FFDDDDDD' } };   
+        }
+    }
+}
+
 
 export async function createSheet(workbook: any) {
     const filePath = path.resolve('xlsPath', `${Date.now()}-test.xls`)
     await workbook.xlsx.writeFile(filePath);
     return filePath
-} 
-
-export async function test() {
-    const workbook = new ExcelJS.Workbook();
-    workbook.creator = 'Me';
-    workbook.lastModifiedBy = 'Her';
-    workbook.created = new Date();
-
-    const worksheet = workbook.addWorksheet('Test');
-    worksheet.columns = [
-        { header: 'ID', key: 'id', width: 20 },
-        { header: 'Data da Venda', key: 'data_venda', width: 20 },
-        { header: 'ID Pedido Marketplace', key: 'id_marketplace', width: 25 },
-        { header: 'Nome do Comprador', key: 'nome_comprador', width: 25 },
-        { header: 'Marketplace', key: 'marketplace', width: 20 },
-        { header: 'ID Integração', key: 'integracao_id', width: 20 },
-        { header: 'Integração', key: 'integracao_name', width: 20 },
-        { header: 'Valor Total da Venda (R$)', key: 'total', width: 25 },{ header: 'Valor do lucro (R$)', key: 'lucro', width: 25 },
-        { header: 'Lucro (%)', key: 'lucro_percent', width: 25 }
-    ];
-
-    const filePath = path.resolve('xlsPath', `${Date.now()}-test.xls`)
-    await workbook.xlsx.writeFile(filePath);
-    return filePath
-
 }
